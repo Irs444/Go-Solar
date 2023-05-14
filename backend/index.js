96bbb9f52f0f7ddc52df95cae547e17520f47acf
Mohammad Mubassir <46520389+m-triple-m@users.noreply.github.com>
2023-05-14 12:03:45 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,45 @@ const cors = require('cors');
 app.use(cors({ origin: 'http://localhost:3000'}));
 const stripe = require("stripe")('sk_test_51N5i2kSE8ALNlcfUeImWOPJjucvuwXy38yixqmADR9BCflGnwkfVUDy2T58YI8FxXSbADNBNK5bkBI4ZSlxSyRNU00guAF4MyK');
 
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
+});
+const connectedExperts = {};
+
+
+io.on("connection", (socket) => {
+  console.log("client connected ");
+  socket.on("addexpert", (id) => {
+    connectedExperts[id] = socket.id;
+    console.log(connectedExperts);
+  });
+
+  socket.on("checkexpert", (id) => {
+    socket.emit("checkexpertfromserver", {
+      status: "online",
+      socketId: connectedExperts[id],
+    });
+  });
+
+  socket.on("sendstudent", (data) => {
+    // console.log(data);
+    data.sent = false;
+    socket.broadcast.emit("recmsg", data);
+  });
+  socket.on("sendmsg", (data) => {
+    console.log(data);
+    data.sent = false;
+    socket.to(data.socketId).emit("recmsg", data);
+  });
+});
+
+
 app.use(express.json());
 
 app.use('/expert', expertRouter);
